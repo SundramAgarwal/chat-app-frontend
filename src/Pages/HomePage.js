4e4782e31e1d85c34218c7559b901f28c6f8a8c9
Sundram Agarwal <agarwal.sundaram@gmail.com>
2023-08-23 16:50:29 +0530

    commit login nd signup add in homepage

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,7 +1,65 @@
 import React from "react";
+import {
+  Box,
+  Container,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
+
+import Login from "../components/Authentication/Login";
+import Signup from "../components/Authentication/Signup";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        textAlign="center"
+        p={3}
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="2xl" fontFamily="work sans" color="black">
+          Welcome to Live-Chat
+        </Text>
+      </Box>
+      <Box
+        p={4}
+        bg="white"
+        w="100%"
+        borderRadius="lg"
+        color="black"
+        borderWidth="1px"
+      >
+        <Tabs variant="soft-rounded">
+          <TabList mb="1rem">
+            <Tab width={"50%"} m="0 3px 0 10rem">
+              Login
+            </Tab>
+            <Tab width={"50%"} m="0 10rem 0 2px">
+              Sign Up
+            </Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
 };
 
 export default HomePage;
