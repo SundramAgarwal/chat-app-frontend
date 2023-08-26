29ff0dc2cc698dd07a673952d5fd27228963b566
Sundram Agarwal <agarwal.sundaram@gmail.com>
2023-08-26 20:33:56 +0530

    GORUP CHAT MODEL CSTART

---
+++ src/components/miscellaneous/UpdateGroupChatModal.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UpdateGroupChatModal = ({ fetchAgain, setFetchAgain }) => {
+  return <div>UpdateGroupChatModal</div>;
+};
+
+export default UpdateGroupChatModal;
